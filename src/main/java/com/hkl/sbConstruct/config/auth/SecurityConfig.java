07606728a17623cc6fd83dd7aa668b9ec29717a8
HKL <bastlwalzer@gmail.com>
2022-11-10 23:10:12 +0900

    구글 로그인기능 구현. (OAuth2)    * https://console.cloud.google.com/

---
+++ src/main/java/com/hkl/sbConstruct/config/auth/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.hkl.sbConstruct.config.auth;
+
+import com.hkl.sbConstruct.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests() //URL별 권한 관리를 위한 옵션의 시작, authorizeRequests이 선언되어야 andMatchers 옵션 사용 가능
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/v1/**").hasRole(Role.USER.name()) //권한 관리 대상 지정 옵션
+                    .anyRequest().authenticated() //설정 값 이외 나머지 URL은 인증된 사용자들에게만 허용하도록.. ( 로그인 )
+                .and()
+                    .logout().logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService); // 소셜 로그인 성 공 후 진행할 UserService 인터페이스 구현체를 등록,
+                                                                                            // 소셜 서비스들에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능을 명시
+    }
+}
---
+++ src/main/java/com/hkl/sbConstruct/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,52 @@
+package com.hkl.sbConstruct.config.auth.dto;
+
+import com.hkl.sbConstruct.domain.user.Role;
+import com.hkl.sbConstruct.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    //OAuth2User에서 반환하는 값이 Map이기 떄문에 값 하나한 변환해야 함.
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    //가입할 때 기본 권한을 GUEST로 주기 위해 사용.
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+
+}
