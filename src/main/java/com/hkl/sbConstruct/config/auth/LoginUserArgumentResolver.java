1ce463c77db529fd298b1deb33235310f5ba1eaa
HKL <bastlwalzer@gmail.com>
2022-11-13 18:00:02 +0900

    매번 생성하던 세션을 인터페이스를 통해 접근하도록 변경

---
+++ src/main/java/com/hkl/sbConstruct/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,40 @@
+package com.hkl.sbConstruct.config.auth;
+
+import com.hkl.sbConstruct.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    /**
+     * 컨트롤러 메서드의 특정 파라미터를 지원하는지 판단,
+     * @LoginUser 어노테이션 붙어있는지 체크
+     * 파라미터 클래스 타입이 SessionUser인지 확인
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return  isLoginUserAnnotation && isUserClass;
+    }
+
+    /**
+     * 파라미터에 전달할 객체를 생성, 여기선 세션에서 객체를 가져온다
+     */
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
