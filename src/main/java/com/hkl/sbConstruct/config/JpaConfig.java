1812ac692cf87df32f9da01d423916a8bbffd7ec
HKL <bastlwalzer@gmail.com>
2022-11-14 00:17:28 +0900

    테스트 케이스에서 @EnableAudition 사용하기 위해선 최소 하나의 Entity가 필요한데.. 그래서 따로 클래스로 분리.

---
+++ src/main/java/com/hkl/sbConstruct/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.hkl.sbConstruct.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
