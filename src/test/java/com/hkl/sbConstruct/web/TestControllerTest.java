817a8aa57f46514e05ffa71fa40c36dbb3980a46
HKL <bastlwalzer@gmail.com>
2022-11-14 00:15:29 +0900

    @WebMvcTest에 excludeFilter 지정    * CustomOAuth2UserService를 읽지 못하여.. SecurityConfig 제외 ( @WebMvcTest 는 @ControllerAdivce, @Controller)만 읽을 수 있음.

--- src/test/java/com/hkl/sbConstruct/web/TestControllerTest.java
+++ src/test/java/com/hkl/sbConstruct/web/TestControllerTest.java
@@ -1,9 +1,13 @@
 package com.hkl.sbConstruct.web;
 
+import com.hkl.sbConstruct.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,13 +16,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) //테스트 수행 시 JUnit에 내장된 실행자 외 다른 실행자(SpringRunner)를 실행 시킴. (SB테스트와 JUnit 사이 연결자 역할)
-@WebMvcTest(controllers = TestController.class) //@Controller 테스트에 사용되는
+@WebMvcTest(controllers = TestController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+}) //@Controller 테스트에 사용되는
 public class TestControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void test1() throws Exception {
         String hello = "hello";
 
